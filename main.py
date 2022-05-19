885a70e6e8b789ec82f99186d9989557342ca7fe
Sabr <57406194+S-a-b-r@users.noreply.github.com>
2022-05-19 22:24:14 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+for i in range(1,101):
+    if(i%3==0):
+        print('Fizz', end='')
+        if(i%5 == 0):
+            print('Buzz')
+        else:
+            print()
+    elif(i%5 == 0):
+        print('Buzz')
+    else:
+        print(i)
+        
